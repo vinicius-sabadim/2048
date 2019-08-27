df96b90f8e61d578ec53ba09fd080eb54df013ca
vinicius-sabadim <vinicius.sabadim@gmail.com>
2019-08-27 13:56:48 +0200

    Doing the logic for the merge

--- src/utils.test.ts
+++ src/utils.test.ts
@@ -1,4 +1,4 @@
-import { isGameOver } from './utils'
+import { isGameOver, mergeCells } from './utils'
 
 describe('utils methods', () => {
   test('is game over when there are no available spaces', () => {
@@ -9,5 +9,88 @@ describe('utils methods', () => {
   test('is not game over when there are available spaces', () => {
     const cells = [{ id: 0, value: 2 }, { id: 1, value: 0 }]
     expect(isGameOver(cells)).toBeFalsy()
+  })
+
+  describe('merging cells', () => {
+    test('should merge cells with the right action', () => {
+      const cells = [
+        { id: 0, value: 2 },
+        { id: 1, value: 2 },
+        { id: 2, value: 0 },
+        { id: 3, value: 2 }
+      ]
+      const merged = [
+        { id: 0, value: 0 },
+        { id: 1, value: 0 },
+        { id: 2, value: 2 },
+        { id: 3, value: 4 }
+      ]
+      const result = mergeCells(cells, 'right')
+      expect(result).toEqual(merged)
+    })
+    test('should merge cells with the left action', () => {
+      const cells = [
+        { id: 0, value: 2 },
+        { id: 1, value: 0 },
+        { id: 2, value: 2 },
+        { id: 3, value: 2 }
+      ]
+      const merged = [
+        { id: 0, value: 4 },
+        { id: 1, value: 2 },
+        { id: 2, value: 0 },
+        { id: 3, value: 0 }
+      ]
+      const result = mergeCells(cells, 'left')
+      expect(result).toEqual(merged)
+    })
+    test('should merge cells with the up action', () => {
+      const cells = [
+        { id: 0, value: 2 },
+        { id: 4, value: 0 },
+        { id: 8, value: 2 },
+        { id: 12, value: 2 }
+      ]
+      const merged = [
+        { id: 0, value: 4 },
+        { id: 4, value: 2 },
+        { id: 8, value: 0 },
+        { id: 12, value: 0 }
+      ]
+      const result = mergeCells(cells, 'up')
+      expect(result).toEqual(merged)
+    })
+    test('should merge cells with the down action', () => {
+      const cells = [
+        { id: 0, value: 2 },
+        { id: 4, value: 2 },
+        { id: 8, value: 0 },
+        { id: 12, value: 2 }
+      ]
+      const merged = [
+        { id: 0, value: 0 },
+        { id: 4, value: 0 },
+        { id: 8, value: 2 },
+        { id: 12, value: 4 }
+      ]
+      const result = mergeCells(cells, 'down')
+      expect(result).toEqual(merged)
+    })
+    test('should merge cells when there are two pairs to be merged', () => {
+      const cells = [
+        { id: 0, value: 2 },
+        { id: 4, value: 2 },
+        { id: 8, value: 2 },
+        { id: 12, value: 2 }
+      ]
+      const merged = [
+        { id: 0, value: 0 },
+        { id: 4, value: 0 },
+        { id: 8, value: 4 },
+        { id: 12, value: 4 }
+      ]
+      const result = mergeCells(cells, 'down')
+      expect(result).toEqual(merged)
+    })
   })
 })
--- src/utils.ts
+++ src/utils.ts
@@ -13,3 +13,38 @@ export function getRandomValue(random = Math.random()): number {
 export function isGameOver(cells: Cell[]): boolean {
   return cells.every(cell => cell.value !== 0)
 }
+
+export function mergeCells(cells: Cell[], action: string): Cell[] {
+  const values = getValuesMerged(cells, action)
+  const sortedValues =
+    action === 'right' || action === 'down'
+      ? values.sort()
+      : values.sort().reverse()
+
+  return cells.map((cell, index) => {
+    return {
+      ...cell,
+      value: sortedValues[index]
+    }
+  })
+}
+
+export function getValuesMerged(cells: Cell[], action: string) {
+  const values = cells.map(cell => cell.value)
+  if (action === 'right' || action === 'down') {
+    for (let index = 3; index > 0; index--) {
+      if (values[index] === values[index - 1]) {
+        values[index] = values[index] * 2
+        values[index - 1] = 0
+      }
+    }
+  } else {
+    for (let index = 0; index < 3; index++) {
+      if (values[index] === values[index + 1]) {
+        values[index] = values[index] * 2
+        values[index + 1] = 0
+      }
+    }
+  }
+  return values
+}
